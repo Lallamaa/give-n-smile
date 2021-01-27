94e879d52988255baf92ac0eba8e5d5059ffb5e0
Yeeshuen Too <66751584+YeeshuenToo@users.noreply.github.com>
2021-01-28 06:18:12 +0800

    header.php

--- us_register.php
+++ us_register.php
@@ -4,16 +4,13 @@
 	include('app/database/connect.php'); 	
     include('functions.php');
     
-<<<<<<< HEAD
     if(isset($_POST['submit']))
     {
-=======
 
->>>>>>> f6227e9175765641b88d196de28d2ef13f167cdd
 		//something was posted
 		$name = $_POST['name'];
 		$password = $_POST['password'];
-<<<<<<< HEAD
+
 		$user_email = $_POST['user_email'];
         $user_phone = $_POST['user_phone'];
         
@@ -26,31 +23,11 @@
             die();
     }
         
-=======
-		
->>>>>>> f6227e9175765641b88d196de28d2ef13f167cdd
-    
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title> Give & Sm:)e | SignUp </title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/query.css">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</head>
-<body>
+    
     <!--Header-->
     <?php include("app/includes/header.php"); ?>
-    
     <div class="container">
         <form method="post">
             <legend class="title text-center">User Sign Up </legend>
@@ -101,3 +78,4 @@
     </div>
     <?php include("app/includes/footer.php"); ?>
 </body>
+</html>
